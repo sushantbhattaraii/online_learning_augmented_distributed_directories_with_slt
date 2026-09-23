102f4798a5fe7be9e878a7ffd206a8b5e4663a1d
Sushant Bhattarai <sushantb2055@gmail.com>
2026-09-23 19:01:41 -0400

    implemented a Shortest path tree algorithm and came up with small world graph results

--- runSmallWorld.sh
+++ runSmallWorld.sh
@@ -1,31 +1,36 @@
-# python launcher.py -n 128small_world_diameter138test.edgelist -c 2 -r 50
-# python launcher.py -n 256small_world_diameter114test.edgelist -c 2 -r 50
-# python launcher.py -n 512small_world_diameter106test.edgelist -c 2 -r 50
-# python launcher.py -n 1024small_world_diameter115test.edgelist -c 2 -r 50
-# python launcher.py -n 2048small_world_diameter143test.edgelist -c 2 -r 50
+python launcher.py -n 128small_world_diameter138test.edgelist -c 2
+python launcher.py -n 256small_world_diameter114test.edgelist -c 2
+python launcher.py -n 512small_world_diameter106test.edgelist -c 2
+python launcher.py -n 1024small_world_diameter115test.edgelist -c 2
+python launcher.py -n 2048small_world_diameter143test.edgelist -c 2
 
-# python launcher.py -n 128small_world_diameter138test.edgelist -c 2.5 -r 50
-# python launcher.py -n 256small_world_diameter114test.edgelist -c 2.5 -r 50
-# python launcher.py -n 512small_world_diameter106test.edgelist -c 2.5 -r 50
-# python launcher.py -n 1024small_world_diameter115test.edgelist -c 2.5 -r 50
+python launcher.py -n 128small_world_diameter138test.edgelist -c 2.5
+python launcher.py -n 256small_world_diameter114test.edgelist -c 2.5
+python launcher.py -n 512small_world_diameter106test.edgelist -c 2.5
+python launcher.py -n 1024small_world_diameter115test.edgelist -c 2.5
+python launcher.py -n 2048small_world_diameter143test.edgelist -c 2.5
 
-# python launcher.py -n 128small_world_diameter138test.edgelist -c 3.3333333333333333333333333333333333333 -r 50
-# python launcher.py -n 256small_world_diameter114test.edgelist -c 3.3333333333333333333333333333333333333 -r 50
-# python launcher.py -n 512small_world_diameter106test.edgelist -c 3.3333333333333333333333333333333333333 -r 50
-# python launcher.py -n 1024small_world_diameter115test.edgelist -c 3.3333333333333333333333333333333333333 -r 50
+python launcher.py -n 128small_world_diameter138test.edgelist -c 3.3333333333333333333333333333333333333
+python launcher.py -n 256small_world_diameter114test.edgelist -c 3.3333333333333333333333333333333333333
+python launcher.py -n 512small_world_diameter106test.edgelist -c 3.3333333333333333333333333333333333333
+python launcher.py -n 1024small_world_diameter115test.edgelist -c 3.3333333333333333333333333333333333333
+python launcher.py -n 2048small_world_diameter143test.edgelist -c 3.3333333333333333333333333333333333333
 
-# python launcher.py -n 128small_world_diameter138test.edgelist -c 5 -r 50
-# python launcher.py -n 256small_world_diameter114test.edgelist -c 5 -r 50
-# python launcher.py -n 512small_world_diameter106test.edgelist -c 5 -r 50
-# python launcher.py -n 1024small_world_diameter115test.edgelist -c 5 -r 50
+python launcher.py -n 128small_world_diameter138test.edgelist -c 5
+python launcher.py -n 256small_world_diameter114test.edgelist -c 5
+python launcher.py -n 512small_world_diameter106test.edgelist -c 5
+python launcher.py -n 1024small_world_diameter115test.edgelist -c 5
+python launcher.py -n 2048small_world_diameter143test.edgelist -c 5
 
-# python launcher.py -n 128small_world_diameter138test.edgelist -c 10 -r 50
-# python launcher.py -n 256small_world_diameter114test.edgelist -c 10 -r 50
-# python launcher.py -n 512small_world_diameter106test.edgelist -c 10 -r 50
-# python launcher.py -n 1024small_world_diameter115test.edgelist -c 10 -r 50
+python launcher.py -n 128small_world_diameter138test.edgelist -c 10
+python launcher.py -n 256small_world_diameter114test.edgelist -c 10
+python launcher.py -n 512small_world_diameter106test.edgelist -c 10
+python launcher.py -n 1024small_world_diameter115test.edgelist -c 10
+python launcher.py -n 2048small_world_diameter143test.edgelist -c 10
 
-# python launcher.py -n 128small_world_diameter138test.edgelist -c inf -r 50
-# python launcher.py -n 256small_world_diameter114test.edgelist -c inf -r 50
-# python launcher.py -n 512small_world_diameter106test.edgelist -c inf -r 50
-# python launcher.py -n 1024small_world_diameter115test.edgelist -c inf -r 50
+python launcher.py -n 128small_world_diameter138test.edgelist -c inf
+python launcher.py -n 256small_world_diameter114test.edgelist -c inf
+python launcher.py -n 512small_world_diameter106test.edgelist -c inf
+python launcher.py -n 1024small_world_diameter115test.edgelist -c inf
+python launcher.py -n 2048small_world_diameter143test.edgelist -c inf
 
